7f99f57403880209cadd388e9ae65a416521d4cf
Cody Johnson <mail@codyjohnson.xyz>
2019-08-01 12:44:02 -0500

    update vim spell

--- .config/fish/functions/sync_d3d-xb.fish
+++ .config/fish/functions/sync_d3d-xb.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.YBwWCD/sync_d3d-xb.fish @ line 2
+# Defined in /tmp/fish.GRlWG4/sync_d3d-xb.fish @ line 2
 function sync_d3d-xb
 	set base_dir $argv[2]
   set output $argv[3]
@@ -8,19 +8,19 @@ function sync_d3d-xb
     
     switch $argv[1]
     case "qb"
-      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_delft3d_includes --include="*/" --exclude="*" cjoh296@qb.loni.org:/work/cjoh296/coupled_delft3d_xbeach.wd/$base_dir/$output /media/cody/sg_external/model_results/coupled_delft3d_xbeach/$base_dir/$output
+      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_delft3d_includes --include="*/" --exclude="*" cjoh296@qb.loni.org:/work/cjoh296/coupled_delft3d_xbeach.wd/$base_dir/$output /media/cody/sg_external/model_results/coupled_delft3d_xbeach/$base_dir
     
     case "smic"
-      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_delft3d_includes --include="*/" --exclude="*" cjoh296@smic.hpc.lsu.edu:/work/cjoh296/coupled_delft3d_xbeach.wd/$base_dir/$output /media/cody/sg_external/model_results/coupled_delft3d_xbeach/$base_dir/$output
+      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_delft3d_includes --include="*/" --exclude="*" cjoh296@smic.hpc.lsu.edu:/work/cjoh296/coupled_delft3d_xbeach.wd/$base_dir/$output /media/cody/sg_external/model_results/coupled_delft3d_xbeach/$base_dir
     end
   
   case "dell-desk"
     switch $argv[1]
     case "qb"
-      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_delft3d_includes cjoh296@qb.loni.org:/work/cjoh296/coupled_delft3d_xbeach.wd/$base_dir/$output /home/cody/Modeling/coupled_delft3d_xbeach/$base_dir/$output
+      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_delft3d_includes cjoh296@qb.loni.org:/work/cjoh296/coupled_delft3d_xbeach.wd/$base_dir/$output /home/cody/Modeling/coupled_delft3d_xbeach/$base_dir
 
     case "smic"
-      rsync -avrz --no-l --include-from=/home/cody/.config/rsync/file_lists/rsync_delft3d_includes cjoh296@smic.hpc.lsu.edu:/work/cjoh296/coupled_delft3d_xbeach.wd/$base_dir/$output /home/cody/Modeling/coupled_delft3d_xbeach/$base_dir/$output
+      rsync -avrz --no-l --include-from=/home/cody/.config/rsync/file_lists/rsync_delft3d_includes cjoh296@smic.hpc.lsu.edu:/work/cjoh296/coupled_delft3d_xbeach.wd/$base_dir/$output /home/cody/Modeling/coupled_delft3d_xbeach/$base_dir
     
     end
   
