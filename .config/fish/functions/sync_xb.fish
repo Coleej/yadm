fa00711ebd64270674283686ca76058d72630c1d
Cody <mail@codyjohnson.xyz>
2019-10-08 14:06:08 -0500

    xbeach stuff

--- .config/fish/functions/sync_xb.fish
+++ .config/fish/functions/sync_xb.fish
@@ -1,12 +1,13 @@
-# Defined in /tmp/fish.osYhAO/sync_xb.fish @ line 2
+# Defined in /tmp/fish.h6wGUW/sync_xb.fish @ line 2
 function sync_xb
+	set mod $argv[2]
 	switch (hostname)
   case "mobile"
     switch $argv[1]
     case "qb"
-      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_xbeach_includes --include="*/" --exclude="*" cjoh296@qb.loni.org:/work/cjoh296/xbeach.wd/models/ /media/cody/sg_external/model_results/xbeach/models
+      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_xbeach_includes --include="*/" --exclude="*" cjoh296@qb.loni.org:/work/cjoh296/xbeach.wd/models/$mod/ /media/cody/sg_external/model_results/xbeach/models/$mod
     case "smic"
-      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_xbeach_includes --include="*/" --exclude="*" cjoh296@smic.hpc.lsu.edu:/work/cjoh296/xbeach.wd/models/ /media/cody/sg_external/model_results/xbeach/models
+      rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_xbeach_includes --include="*/" --exclude="*" cjoh296@smic.hpc.lsu.edu:/work/cjoh296/xbeach.wd/models/$mod/ /media/cody/sg_external/model_results/xbeach/models/$mod
     case "local"
       rsync -avrz --include-from=/home/cody/.config/rsync/file_lists/rsync_xbeach_includes --include="*/" --exclude="*" /home/cody/Phd/40_modeling/xbeach/models/ /media/cody/sg_external/model_results/xbeach/models
     end
