4c69375ca36f3471f07e47c0b4aa7d6e63db2e2c
Cody <mail@codyjohnson.xyz>
2019-10-21 12:20:49 -0500

    fpass function

--- .config/fish/functions/fpass.fish
+++ .config/fish/functions/fpass.fish
@@ -1,5 +1,6 @@
-function fpass -d "Fuzzy-find a Lastpass entry and copy the password"
-  if not lpass status -q
+# Defined in /tmp/fish.BF762c/fpass.fish @ line 2
+function fpass --description 'Fuzzy-find a Lastpass entry and copy the password'
+	if not lpass status -q
     lpass login $EMAIL
   end
 
@@ -8,4 +9,15 @@ function fpass -d "Fuzzy-find a Lastpass entry and copy the password"
   end
 
   lpass ls | fzf | string replace -r -a '.+\[id: (\d+)\]' '$1' | read -l result; and lpass show -c --password "$result"
+
+  read -l -P 'Print account information [y/N]? ' confirm
+
+  switch $confirm
+    case Y y
+      lpass show -- all "$result"
+      commandline -f repaint
+      return 0
+    case '' N n
+      commandline -f repaint
+  end
 end
