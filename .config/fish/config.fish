70a6a3f838d568ae1eb773a2081c70474e6c6361
Cody Johnson <mail@codyjohnson.xyz>
2021-07-01 08:19:59 -0500

    better variable assignments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,8 +6,8 @@ set -x XDG_DATA_DIRS $XDG_DATA_DIRS:/var/lib/snapd/desktop
 
 ## virtualfish
 set -x VIRTUALFISH_DEFAULT_PYTHON "/usr/bin/python3"
-set -x VIRTUALFISH_HOME "~/.config/virtualenvs"
-set -x PROJECT_HOME "~/Projects"
+set -x VIRTUALFISH_HOME "$XDG_CONFIG_HOME/virtualenvs"
+set -x PROJECT_HOME "$HOME/Projects"
 
 ## taskwarrior
 set -x TASKRC "$XDG_CONFIG_HOME/task/taskrc"
