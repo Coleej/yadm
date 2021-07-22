116174cc7b59d8231b5bf3544c5c9c692b4ee71a
Cody Johnson <mail@codyjohnson.xyz>
2021-07-21 22:27:33 -0500

    bwcopy is working but needs refinement

--- .config/fish/functions/bwcopy.fish
+++ .config/fish/functions/bwcopy.fish
@@ -1,13 +1,22 @@
 function bwcopy
-  if command -sq bw and command -sq fzf-tmux and command -sq xclip
+  set pw fmb777999
+
+  if not set -q BW_SESSION
+    bw login mail@codyjohnson.xyz $pw >/dev/null 2>&1 
+    bw unlock $pw > /tmp/bw.txt
+    sed -nE 's|.*="(.*)"|\1|pi' /tmp/bw.txt | head -1 | read BW_SESSION
+    set -x BW_SESSION $BW_SESSION
+  end
+
+  if command -sq bw and command -sq fzf and command -sq xclip
         bw get item (
             bw list items \
             | jq -r '.[] | [.name, .login.username // "", .id] | @tsv' \
             | column -t -s \t \
-            | fzf-tmux --with-nth 1..-2 --preview-window down:2 \
+            | fzf --with-nth 1..-2 --preview-window down:2 \
                        --preview 'bw get item (echo {} | awk \'{print $NF}\') | jq -r \'.login.uris | .[] | .uri // empty \'' \
             | awk '{print $NF}'
-        ) | jq -r '.login.password' | xclip -selection primary
+        ) | jq -r '.login.password' | xsel -i -b
     end
 end
 
