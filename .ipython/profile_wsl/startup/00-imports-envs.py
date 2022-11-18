74c2d03ae5b667a4a8b8bc50fb0b54a8b636611f
Cody Johnson <Cody.Johnson@mottmac.com>
2022-11-18 15:38:01 -0600

    mostly new prompt for ipython

--- .ipython/profile_wsl/startup/00-imports-envs.py
+++ .ipython/profile_wsl/startup/00-imports-envs.py
@@ -10,17 +10,8 @@ import numpy as np
 import pandas as pd
 import scipy as sci
 import sympy as sym
-from numpy import *
 from numpy import e, pi
-from pyproj import Proj, transform
-
 from scipy.constants import *
 from spellchecker import SpellChecker
 
 spellcheck = SpellChecker()
-
-wgs84 = Proj("epsg:4326")
-wgs84_merc = Proj("epsg:3857")
-nad83_grs80 = Proj("epsg:4269")
-
-utm15n = Proj("epsg:26915")
