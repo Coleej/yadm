e73f46e76b6302afa398f2a301c5fae31cb3cc19
Cody Johnson <Cody.Johnson@mottmac.com>
2023-07-07 23:05:46 -0500

    basic lsp seems to work

--- .config/nvim/after/plugin/zero-lsp.lua
+++ .config/nvim/after/plugin/zero-lsp.lua
@@ -1,10 +1,25 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+	lsp.default_keymaps({ buffer = bufnr })
+	lsp.buffer_autoformat()
+
+	vim.keymap.set({ "n" }, "U", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true })
+	vim.keymap.set({ "n", "x" }, "<F9>", function()
+		vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+	end, { buffer = bufnr })
 end)
 
+--[[
+lsp.set_sign_icons({
+	error = "✘",
+	warn = "▲",
+	hint = "⚑",
+	info = "»"
+})
+--]]
+
 -- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
