a363e3c23db18ee3dfef7095b2c0cce65d12cbec
Cody Johnson <Cody.Johnson@mottmac.com>
2023-07-14 12:18:58 -0500

    nerd fonts

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -2,8 +2,8 @@ require("lualine").setup {
 	options = {
 		icons_enabled = true,
 		theme = "material",
-		component_separators = { left = "", right = " " },
-		section_separators = { left = " ", right = " " },
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = { "NvimTree" },
 			winbar = { "NvimTree" },
