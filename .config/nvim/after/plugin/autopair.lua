a54bb322f4fcba82d430e443e1575fa2cc49e37f
Cody Johnson <Cody.Johnson@mottmac.com>
2023-07-26 21:07:45 -0500

    fixed autopair rule for %

--- .config/nvim/after/plugin/autopair.lua
+++ .config/nvim/after/plugin/autopair.lua
@@ -2,6 +2,9 @@ local Rule = require("nvim-autopairs.rule")
 local npairs = require("nvim-autopairs")
 npairs.setup {}
 
-npairs.add_rule(Rule("%", "%", "htmldjango"))
+npairs.add_rules({
+	Rule("%", "%", "htmldjango")
+			:with_move(function(opts) return opts.char == "%" end)
+})
 
 require("nvim-ts-autotag").setup {}
