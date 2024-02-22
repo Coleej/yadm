1a8b705d029ba0b3932e2128ece909b700653026
Cody Johnson <codyjohnson144@gmail.com>
2024-02-22 09:27:29 -0600

    nvim-tree and git

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -5,9 +5,25 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
+-- custom mappings on attach
+local function my_on_attach(bufnr)
+	local api = require "nvim-tree.api"
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
 -- OR setup with some options
 require("nvim-tree").setup({
 	sort_by = "case_sensitive",
+	on_attach = my_on_attach,
 	view = {
 		width = 50,
 	},
